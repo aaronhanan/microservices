a62a8e15c61ab7591479404d8c160b15b8dc7dc4
test account <harish7673@hotmail.com>
2021-04-14 10:58:19 -0700

    nginx

--- receiver/app.py
+++ receiver/app.py
@@ -94,7 +94,7 @@ def report_scheduled_order(body):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yml", base_path="/receiver", strict_validation=True, validate_responses=True)
 
 
 if __name__ == "__main__":
